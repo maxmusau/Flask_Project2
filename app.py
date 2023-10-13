6efb0952f29537b0ba5960955e6bb10e2d92a37f
maxmusau <musaumaxwell@gmail.com>
2023-10-13 11:08:29 +0300

    signin

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import *
 import pymysql
 # create the app
 app=Flask(__name__)
+# define the secret key
+app.secret_key='jjgdjgaj$%#&@^LJKGDSFD'
 # define the connection
 connection=pymysql.connect(host="localhost",user="root",database="bookings_db",password="")
 # create the main
+
 @app.route("/")
 def Main():
     return "Welcome to our application. Type the route name in the address bar"
@@ -12,6 +15,8 @@ def Main():
 # create the room-upload route
 @app.route("/upload_room",methods=['GET','POST'])
 def Upload_room():
+    if 'key_admin' not in session:
+        return redirect("/admin")
     # check if user has posted anything
     if request.method == 'POST':
         # TODO
@@ -109,11 +114,104 @@ def Signup():
             # commit
              connection.commit()
              return render_template("signup.html",message="Signup Successful")
-             
     else:
         return render_template("signup.html")
-    
 # signin
+@app.route("/login",methods=['GET','POST'])
+def Signin():
+    # check the method
+    if request.method=='POST':
+        # TODO
+        username=request.form['username']
+        password=request.form['password']
+        # define the sql query
+        sql='select * from users where username=%s and password=%s'
+        # create cursor functon
+        cursor=connection.cursor()
+        # execute the sql
+        cursor.execute(sql,(username,password))
+        # check if user exists
+        if cursor.rowcount==0:
+            return render_template("signup.html",error="Incorrect login credentials. Try again")
+        # create the user sessions
+        session['key']=username
+        # fetch the other columns
+        user=cursor.fetchone()
+        session['email']=user[1]
+        session['phone']=user[2]
+        return redirect("/getrooms")   
+    else:
+        return render_template("signup.html")
+# clear the session
+@app.route("/logout")
+def Logout():
+    session.clear()
+    return redirect("/login")
+
+@app.route("/logout_admin")
+def Logout_admin():
+    session.clear()
+    return redirect("/admin")
+
+# login route for admin
+@app.route("/admin",methods=['GET','POST'])
+def Admin():
+    # check the method
+    if request.method=='POST':
+        # TODO
+        username=request.form['username']
+        password=request.form['password']
+        # define the sql query
+        sql='select * from users where username=%s and password=%s'
+        # create cursor functon
+        cursor=connection.cursor()
+        # execute the sql
+        cursor.execute(sql,(username,password))
+        # check if user exists
+        if cursor.rowcount==0:
+            return render_template("admin.html",error="Incorrect login credentials. Try again")
+        admin=cursor.fetchone()
+        role=admin[0]
+        if role != 'admin':
+            return render_template("admin.html",error="Incorrect login credentials. Try again")
+        # create the user sessions
+        session['key_admin']=username
+        # fetch the other columns
+        # user=cursor.fetchone()
+        # print(user)
+        session['email_admin']=admin[1]
+        session['phone_admin']=admin[2]
+        return redirect("/upload_room")   
+    else:
+        return render_template("admin.html")
+
+# single item route
+@app.route("/single_room/<room_id>",methods=['GET','POST'])
+def Single_item(room_id):
+    # check if user has signed in/session is live
+    if 'key' not in session:
+        return redirect("/login")
+    else:
+        # define thee sql query
+        sql='select * from rooms where room_id=%s'
+        # create the cursor function 
+        cursor=connection.cursor()
+        # execute the sql query
+        cursor.execute(sql,room_id)
+        # check if product exists
+        if cursor.rowcount==0:
+            return render_template("single_item.html",error="Prodcut does not exist")
+        # fetch the product info
+        room=cursor.fetchone()
+        # return the variable room
+        # return jsonify(room)
+        return render_template("single_item.html",data=room)
+    
+        
+        
+    pass
+    
+    
 
 
 # run theh app
